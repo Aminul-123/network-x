46438c073930d84c55ab1875a651d2f87fa27802
Aminul Ali <alomamin963@gmail.com>
2024-10-17 09:41:12 +0530

    repost btn

--- src/assets/Setting.jsx
+++ src/assets/Setting.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removePost, savePost } from "../slices/postSlice";
-import { deletePost } from "../services/getPostData";
+import { createPost, deletePost } from "../services/getPostData";
 
 function Setting({ post }) {
   //TOMMOROW WE WILL BE WORKING ON SAVE POST BTN , POST WILL BE SAVED IN REDUX STORE POST=[],
   // LATER ON WE WILL SHOW UP THESE POST WHEN IN MY ITEMS BTN IS CLICKED.
   const dispatch = useDispatch();
+  const userName = useSelector((state) => state?.post?.username);
 
   const isSaved = useSelector((state) =>
     state?.post?.savedPost.find((item) => item.id === post.id)
@@ -22,6 +23,19 @@ function Setting({ post }) {
     await deletePost(post.id);
     console.log("post deleted successfully");
   }
+ async function handleRepost () {
+    const repost = {
+        id : crypto.randomUUID(), 
+        name : userName, // name is username
+        date_created : new Date().toDateString(),
+        heading : post.heading || '',
+        description : post.description,
+        imageUrl : post.imageUrl,
+        likes : 0,
+        comments : 0
+    }
+    await createPost(repost);
+  }
   return (
     <div className="relative right-40">
       <div className="w-40 p-2 bg-gray-100 border border-grey-700 shadow-md shadow-gray-500 absolute ">
@@ -33,7 +47,9 @@ function Setting({ post }) {
               <p onClick={handleSavePost}>Save Post</p>
             )}
           </li>
-          <li className="list-style">Repost</li>
+          <li className="list-style"
+          onClick={handleRepost}
+          >Repost</li>
           <li className="list-style" onClick={handleDeletePost}>
             Delete Post
           </li>
